be8ed6f8dbf2747cf67047dbfd70806c0f3edd6f
Lionel Beato <lionel.beato@gmail.com>
2021-04-15 11:55:21 -0400

    adds day 9.4

---
+++ week-9/9-4/README.md
@@ -0,0 +1,13 @@
+# Day 9.4 - Java Data Structures
+
+_Today we covered major Java data structures such as those in the Collection framework, stacks and queues, and others._
+
+## Resources 
+
+- [Intro to Collections | *Oracle Java Tutorial* ](https://docs.oracle.com/javase/tutorial/collections/intro/index.html)
+  
+- [Java Collections Portal | *Baeldung*](https://www.baeldung.com/java-collections)
+  
+- [Java Stack | *Baeldung*](https://www.baeldung.com/java-stack)
+  
+- [What is a node?](https://stackoverflow.com/questions/15725314/how-do-nodes-work-in-java)
---
+++ week-9/9-4/src/main/java/com/tts/Main.java
@@ -0,0 +1,119 @@
+package com.tts;
+
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] fruitArray = {"orange", "apple", "dragonfruit"};
+        int[] intArray;
+        boolean[] boolArray;
+
+        System.out.println("==== Arrays ====");
+        System.out.println("Element at index 1: " + fruitArray[1]);
+        System.out.println("Length of fruitArray: " + fruitArray.length);
+
+        System.out.println("==== Collections ====");
+
+        List<String> stringList = new ArrayList<>();
+        List<String> stringLinkedList = new LinkedList<>();
+        List<String> stringVector = new Vector<>();
+        List<String> stringStack = new Stack<>();
+
+        stringList.add("dog");
+        stringList.add("house");
+        stringList.add("cell phone");
+
+        // {dog, house, cell phone}
+
+        stringList.add(1, "cat");
+
+        // {dog, cat, house, cell phone}
+
+        System.out.println("The element at index 0: " + stringList.get(1));
+
+        stringList.remove(0);
+        System.out.println("This is the size of my list: " + stringList.size());
+
+        // this is completely remove all elements from your list
+//        stringList.clear();
+
+        Collections.reverse(stringList);
+        System.out.println("Here is my list reversed: " + stringList);
+
+        System.out.println("==== String Conversion ====");
+
+        // first I set up a String
+        String myString = "I really love ice cream";
+        // then I take my string and split it at every whitespace
+        // and turn it into a regular array
+        String[] strArr = myString.split(" ");
+
+        // setting up arrayList and making the value
+        // that of the strArr
+        List<String> splitList = Arrays.asList(strArr);
+
+        System.out.println(splitList);
+
+        List<String> carList = Arrays.asList("BMW", "Honda", "Audi");
+        Object[] carArr = carList.toArray();
+        String carString = Arrays.toString(carArr);
+
+        System.out.println(carString.replace("[", "")
+        .replace("]", "")
+        .replace(",", " "));
+
+
+        System.out.println("==== Hashmap ====");
+
+        HashMap<String, Integer> personMap = new HashMap<>();
+
+        personMap.put("Mary", 37);
+        personMap.put("Bob", 21);
+        personMap.put("Susan", 63);
+
+        System.out.println(personMap);
+        System.out.println("This is the value associated with Mary: " + personMap.get("Mary"));
+        System.out.println(personMap.keySet());
+
+        for (String i : personMap.keySet()) {
+            System.out.println("key: " + i + "\tvalue: " + personMap.get(i));
+        }
+
+        personMap.remove("Bob");
+        System.out.println("Is Bob in my map? "+ personMap.containsKey("Bob"));
+
+        System.out.println("==== Iterating ====");
+
+        List<String> fruitList = Arrays.asList("plum", "grape", "fiji apple");
+
+        // this is referred to as an enhanced for loop
+        // enhanced for loops are strictly used for collections
+        // String fruit represents any given element in our fruitList collection
+        for (String fruit : fruitList) {
+            System.out.println("Here is one of my fruits: " + fruit);
+        }
+
+        fruitList.forEach((fruit) -> System.out.println(fruit));
+        fruitList.forEach(System.out::println);
+
+        System.out.println("==== LinkedList ====");
+
+        LinkedList<String> animalList = new LinkedList<>();
+        animalList.add("frog");
+        animalList.add("giraffe");
+        animalList.add("buffalo");
+        animalList.add("mongoose");
+        System.out.println(animalList);
+
+        animalList.addFirst("shark");
+        System.out.println(animalList);
+
+        animalList.set(1, "emu");
+        System.out.println(animalList);
+
+
+    }
+}
