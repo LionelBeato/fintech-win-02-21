dc4d8e36f741d6d66c7406b6d7771ec48372f1f3
Lionel Beato <lionel.beato@gmail.com>
2021-05-07 09:42:25 -0400

    wraps up lesson for generics

--- week-12/day-5/src/main/java/com/tts/day125/generic/Main.java
+++ week-12/day-5/src/main/java/com/tts/day125/generic/Main.java
@@ -1,10 +1,32 @@
 package com.tts.day125.generic;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+class A {}
+class B extends A {}
+class C extends B {}
+class D extends B {}
+
 public class Main {
+
+    // you can extend a wild as well
+    public void otherMethod(List<? extends B> lb) {
+        B b = lb.get(0);
+    }
+    public static void handleNumbers(List<? extends Number> numbers) {
+        System.out.println(numbers);
+    }
+
+    public static void printSuperClass(List<? super Integer> list) {
+        System.out.println(list);
+    }
+
+    public static void printSuperClassTwo(List<? super B> list) {
+        System.out.println(list);
+    }
 
     // this is acting as a regular method
     // but the return type is set up as a generic
@@ -12,6 +34,20 @@ public class Main {
     public static <W> void printList(List<W> list) {
         for (W element: list) {
             System.out.println(element);
+        }
+    }
+
+    // below is the wildcard ? generic
+    // this acts similarly to utilizing a single named generic
+    // however, it doesn't enforce types having to be the same
+    // and you cannot return something that is of type ?
+    public static void printCollection(Collection<?> c, Collection<?> d) {
+        for (Object e : c) {
+            System.out.println(e);
+        }
+
+        for (Object e : d) {
+            System.out.println(e);
         }
     }
 
@@ -43,7 +79,6 @@ public class Main {
         integerList.add(3);
         integerList.add(645);
 
-        //
         List rawList = new ArrayList<>();
 
 
@@ -64,6 +99,19 @@ public class Main {
         printTwoGenerics(2000L, "this is a string omg");
 
         GenericClass<Integer> myGenericClass = new GenericClass<>(3, "Bob");
+
+        printCollection(integerList, stringList);
+
+        printSuperClass(Arrays.asList(1,2,3,4,5));
+        // we can have a higher bound generic that will accept all legal parent classes
+        // since object is a parent to integer and we are using the super keyword
+        // the code below will resolve
+        printSuperClass(Arrays.asList(new Object(), new Object()));
+        printSuperClassTwo(Arrays.asList(new D(), new D()));
+
+        // the generic is bound to all types of type Number
+        // we can only use Number, Integer, Float, Double, Long...
+//        handleNumbers(Arrays.asList("Hello"));
 
     }
 }
