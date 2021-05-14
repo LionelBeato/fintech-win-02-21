98f9239e9fe617279cfee46c2979eb2e37d6bca0
Lionel Beato <lionel.beato@gmail.com>
2021-05-14 10:14:44 -0400

    finish project

--- week-13/Spring-Boot-Start/src/main/java/com/tts/Spring/Boot/Start/configuration/SecurityConfig.java
+++ week-13/Spring-Boot-Start/src/main/java/com/tts/Spring/Boot/Start/configuration/SecurityConfig.java
@@ -1,5 +1,24 @@
 package com.tts.Spring.Boot.Start.configuration;
 
 
-public class SecurityConfig {
+import org.apache.catalina.startup.WebappServiceLoader;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/h2-console/**").permitAll();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
 }
