d11017ad39c1734efc4c67adf2f1b48787a8c311
Lionel Beato <lionel.beato@gmail.com>
2021-05-18 11:34:13 -0400

    fix for static resources

--- Week-14/week14day2/authentication/src/main/java/com/tts/authentication/configuration/MvcConfig.java
+++ Week-14/week14day2/authentication/src/main/java/com/tts/authentication/configuration/MvcConfig.java
@@ -1,6 +1,8 @@
 package com.tts.authentication.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
--- Week-14/week14day2/authentication/src/main/java/com/tts/authentication/configuration/WebSecurityConfig.java
+++ Week-14/week14day2/authentication/src/main/java/com/tts/authentication/configuration/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.tts.authentication.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -16,8 +17,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http.authorizeRequests()
                 .antMatchers("/", "home").permitAll()
+                .antMatchers("/**/*.js", "/**/*.css").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
