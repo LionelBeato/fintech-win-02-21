91c0b3ac22d59ae53b44a5b520c61a0f8cb9d1aa
Lionel Beato <lionel.beato@gmail.com>
2021-05-18 15:01:44 -0400

    add oauth project

---
+++ Week-14/week14day2/oauth2Demo/src/test/java/com/tts/oauth2Demo/Oauth2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.oauth2Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
