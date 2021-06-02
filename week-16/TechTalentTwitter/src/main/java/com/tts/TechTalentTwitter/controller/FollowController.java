f3b6c85fa85176fd8a81f91934b79c66db3d2396
Lionel Beato <lionel.beato@gmail.com>
2021-06-02 09:56:21 -0400

    implements following and followers

---
+++ week-16/TechTalentTwitter/src/main/java/com/tts/TechTalentTwitter/controller/FollowController.java
@@ -0,0 +1,44 @@
+package com.tts.TechTalentTwitter.controller;
+
+import com.tts.TechTalentTwitter.model.User;
+import com.tts.TechTalentTwitter.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class FollowController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @PostMapping("/follow/{username}")
+    public String follow(@PathVariable("username") String username,
+                         HttpServletRequest request) {
+        User loggedInUser = userService.getLoggedInUser();
+        User userToFollow = userService.findByUserName(username);
+        List<User> followers = userToFollow.getFollowers();
+        followers.add(loggedInUser);
+        userToFollow.setFollowers(followers);
+        userService.save(userToFollow);
+        return "redirect: " + request.getHeader("Referer");
+    }
+
+    @PostMapping("/unfollow/{username}")
+    public String unfollow(@PathVariable("username") String username,
+                         HttpServletRequest request) {
+        User loggedInUser = userService.getLoggedInUser();
+        User userToUnfollow = userService.findByUserName(username);
+        List<User> followers = userToUnfollow.getFollowers();
+        followers.remove(loggedInUser);
+        userToUnfollow.setFollowers(followers);
+        userService.save(userToUnfollow);
+        return "redirect: " + request.getHeader("Referer");
+    }
+
+}
