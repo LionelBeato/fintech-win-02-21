432b4b620048076e12f2b6a92d9e95a15047e326
Lionel Beato <lionel.beato@gmail.com>
2021-07-20 13:28:49 -0400

    add comments

--- capstone/fullstack-demo/backend-demo/src/test/java/com/tts/fullstackdemo/FullstackDemoApplicationTests.java
+++ capstone/fullstack-demo/backend-demo/src/test/java/com/tts/fullstackdemo/FullstackDemoApplicationTests.java
@@ -3,6 +3,9 @@ package com.tts.fullstackdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// we need to comment the annotation out because
+// this will attempt to provision a database
+// which is impossible with our github actions workflow
 //@SpringBootTest
 class FullstackDemoApplicationTests {
 
