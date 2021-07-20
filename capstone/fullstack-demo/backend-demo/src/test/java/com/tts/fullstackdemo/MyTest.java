229dcacda5151d8aecc7cea2b67fc980772b3ba7
Lionel Beato <lionel.beato@gmail.com>
2021-07-20 13:42:36 -0400

    wrote amazing test

---
+++ capstone/fullstack-demo/backend-demo/src/test/java/com/tts/fullstackdemo/MyTest.java
@@ -0,0 +1,15 @@
+package com.tts.fullstackdemo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class MyTest {
+
+    @Test
+    public void test() {
+        assertTrue(false);
+    }
+
+}
